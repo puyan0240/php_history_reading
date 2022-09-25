d6a0ff1742bb1e7d1c44632354f372f342c106e4
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-09-25 19:39:02 +0900

    DBアクセス用のID/ﾊﾟｽを環境変数から取得

--- php/content/common/DbManager.php
+++ php/content/common/DbManager.php
@@ -1,14 +1,21 @@
 <?php
-function getDb($dbname, $host, $user, $passwd) : PDO
+function getDb($dbname, $host) : PDO
 {
+    //環境変数(.env)からユーザーIDとパスワード取得
+    {
+        require dirname(__FILE__).'/../vendor/autoload.php'; //vendorディレクトリの階層を指定する
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__. '/..'); //.envの階層を指定する
+        $dotenv->load();
+
+        $user=$_ENV['USER_ID'];
+        $passwd=$_ENV['USER_PASS'];
+    }
+
    //データベースへのアクセス
     try
     {
         $format = 'mysql:dbname=%s; host=%s; charset=utf8';
         $dsn = sprintf($format, $dbname, $host);
-        #echo $dsn;
-        #echo $user;
-        #echo $passwd;
 
         $db = new PDO($dsn, $user, $passwd);
         return $db;

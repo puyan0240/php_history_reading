75b58386d644e3b416dffe630a01407f298c9cec
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-10-10 11:11:06 +0900

    DBアクセスのDB名とHOSTは.envから取得するよう変更

--- php/content/common/DbManager.php
+++ php/content/common/DbManager.php
@@ -1,13 +1,19 @@
 <?php
-function getDb($dbName, $host) : PDO {
+
+#-----------------------------------------------------------
+# DBアクセス関数
+#-----------------------------------------------------------
+function getDb() : PDO {
     //環境変数(.env)からユーザーIDとパスワード取得
     {
         require dirname(__FILE__).'/../vendor/autoload.php'; //vendorディレクトリの階層を指定する
         $dotenv = Dotenv\Dotenv::createImmutable(__DIR__. '/..'); //.envの階層を指定する
         $dotenv->load();
 
-        $user=$_ENV['USER_ID'];
-        $passwd=$_ENV['USER_PASS'];
+        $dbName = $_ENV['DB_NAME'];     //DB名
+        $host   = $_ENV['DB_HOST'];     //ホスト
+        $user   = $_ENV['USER_ID'];     //ユーザー名
+        $passwd = $_ENV['USER_PASS'];   //ユーザーパスワード
     }
 
    //データベースへのアクセス
@@ -22,18 +28,26 @@ function getDb($dbName, $host) : PDO {
     catch (PDOException $e)
     {
         echo "接続エラー:".$e->getMessage();
-        //die("接続エラー : {$e->getMessage()}");
         return null;
     }
 }
 
-function insertDb($dbName, $host, $tblName, $keyValue) {
 
-    $db = getDb($dbName, $host);
+#-----------------------------------------------------------
+# テーブルに新規追加 (INSERT INTO)
+#-----------------------------------------------------------
+function insertDb($tblName, $keyValue) {
+
+    $result = FALSE;
+
+    //DBアクセス
+    $db = getDb();
     if ($db != null) {
+
         $str_tblElement = "";
         $str_bindName = "";
 
+        //引数の連想配列は、テーブルの 要素名:値 になっている
         foreach ($keyValue as $key => $value) {
             $str_tblElement .= ($key.", ");
             $str_bindName .= (":".$key.", ");
@@ -41,6 +55,7 @@ function insertDb($dbName, $host, $tblName, $keyValue) {
         $str_tblElement = rtrim($str_tblElement, ", ");
         $str_bindName = rtrim($str_bindName, ", ");
  
+        //SQLのINSERT INTOを行う
         try {
             $format = 'INSERT INTO %s(%s) VALUES(%s)';
             $str_sql = sprintf($format, $tblName, $str_tblElement, $str_bindName);
@@ -55,13 +70,12 @@ function insertDb($dbName, $host, $tblName, $keyValue) {
         }
         catch (PDOException $e) {
             echo "err:".$e->getMessage()."<br>";
-            var_dump($db->errorInfo());
         }
 
         $db = null;
-        return TRUE;
-    } else {
-        return FALSE;
+        $result = TRUE;
     }
+
+    return $result;
 }
 ?>

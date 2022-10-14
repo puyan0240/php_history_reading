c974b1a69eab10c1ca3059ba816dd25b960551ea
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-10-14 23:40:54 +0900

    編集画面の仮実装

---
+++ php/content/edit.php
@@ -0,0 +1,80 @@
+<?php
+    require_once './common/DbManager.php';
+
+    $tblName = "history_book_tbl";
+    $title = $author = $publisher = $recommend = $comment = "";
+
+    $idx = $_GET['idx'];
+    //echo $idx;
+
+    //DB TABLEから読み出し
+    $param = 'idx ='.$idx;
+    $ret = getFromTbl($tblName, $param);
+    if ($ret != FALSE) {
+        foreach ($ret as $value) {
+            $date      = $value['date'];
+            $title     = $value['title'];
+            $author    = $value['author'];
+            $publisher = $value['publisher'];
+            $recommend = $value['recommend'];
+            $comment   = $value['comment'];
+        }
+    }
+    //select option の初期値
+    $selectedTbl = [NULL,"","","","",""];
+    $selectedTbl[$recommend] = "selected";
+    //var_dump($selectedTbl);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>編集</title>
+</head>
+<body>
+    <form action="add_confirm.php" method="POST">
+        <div>
+            <p>編集</p>
+            <table>
+                <tr>
+                    <td>タイトル:</td>
+                    <td><input type="text" name="title" value="<?php echo $title;?>"></td>
+                </tr>
+                <tr>
+                    <td>著者:</td>
+                    <td><input type="text" name="author" value="<?php echo $author;?>"></td>
+                </tr>
+                <tr>
+                    <td>出版社:</td>
+                    <td><input type="text" name="publisher" value="<?php echo $publisher;?>"></td>
+                </tr>
+                <tr>
+                    <td>評価:</td>
+                    <td>
+                        <select name="recommend">
+                            <option value="1" <?php echo $selectedTbl[1];?>>1</option>
+                            <option value="2" <?php echo $selectedTbl[2];?>>2</option>
+                            <option value="3" <?php echo $selectedTbl[3];?>>3</option>
+                            <option value="4" <?php echo $selectedTbl[4];?>>4</option>
+                            <option value="5" <?php echo $selectedTbl[5];?>>5</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>コメント:</td>
+                    <td><input type="text" name="comment" value="<?php echo $comment;?>"></td>
+                </tr>
+            </table>
+
+            <input type="reset" value="取消">
+            <input type="submit" value="登録確認">
+        </div>
+    </form>
+    <div>
+        <a href="index.php">戻る</a>
+    </div>
+</body>
+</html>

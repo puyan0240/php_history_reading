e0c9e7e9e0b2b915dc2733dcbbaab3639aeec760
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-10-16 16:37:40 +0900

    エクセルへのエクスポート作成

--- php/content/data_export.php
+++ php/content/data_export.php
@@ -6,52 +6,65 @@
     use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 
-    $tblName = "history_book_tbl";
+    //エクスポートするファイル名
+    $file_name = "data_backup.xlsx";
+
 
     //DB TABLEから読み出し(全件取得)
+    $tblName = "history_book_tbl";
     $ret = getFromTbl($tblName, NULL);
     if ($ret == FALSE) {
         //失敗
         exit();
     }
 
-    //DB 結果出力
-    foreach ($ret as $line) {
-        foreach ($line as $key => $value) {
-            //echo $key."=".$value."<br>";
-        }
-    }
 
-
-
-
-/*
     //Spreadsheetオブジェクト作成
     $objSpreadsheet = new Spreadsheet();
 
     //シート設定
     $objSheet = $objSpreadsheet->getActiveSheet();
+    
+    //1行目作成
+    $itemTbl = ['No.','Date', 'Title', 'Author', 'Publisher', '★', 'Comment'];
+    $col = 'A';
+    $row = 1;
+    foreach ($itemTbl as $Name) {
+        $cell = $col.$row;
+        $objSheet->setCellValue($cell, $Name);    
 
-    //[A1]セルに文字列
-    $objSheet->setCellValue('A1', 'Spreadsheet');
+        $col ++;    //次のColumへ
+    }
 
-    //[A2]セルに文字列
-    $objSheet->setCellValue('A2', '123.56');
+    //DB 結果出力を２行目以降に
+    $count = 1;
+    foreach ($ret as $line) {
+        $col = 'A';
+        $row ++;
+        foreach ($line as $key => $value) {
+            $cell = $col.$row;
 
-    //[A3]セルに文字列
-    $objSheet->setCellValue('A3', 'True');
-
+            if ($key == 'idx') {
+                $objSheet->setCellValue($cell, $count);
+            }
+            else {
+                $objSheet->setCellValue($cell, $value);
+            }          
+            $col ++;
+        }
+        $count ++;
+    }
 
     //ブラウザへの指定
-    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    header('Content-Disposition: attachment;filename="test.xlsx"');
-    header('Cache-Control: max-age=0');
+    header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    header("Content-Disposition: attachment;filename={$file_name}");
+    header("Cache-Control: max-age=0");
 
     //XLSX形式オブジェクト生成
     $objWriter = new Xlsx($objSpreadsheet);
 
     //ファイル書き込み
     $objWriter->save('php://output');
-*/
+
     exit();
 ?>

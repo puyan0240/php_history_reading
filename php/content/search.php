4ea6e300543c1882244b4bd2886279df285919be
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-11-03 11:16:39 +0900

    検索結果表示を実装

--- php/content/search.php
+++ php/content/search.php
@@ -1,9 +1,54 @@
 <?php
     require_once './common/DbManager.php';
-
-    //var_dump($_POST);
+    require_once './common/Encode.php';
 
     $tblName = "history_book_tbl";
+
+    
+    //検索結果
+    {
+        $strSearchResult = "";
+        $format = "
+        <tr>
+            <td><button onclick=\"location.href='branch.php?edit_type=disp&idx=%d'\">閲覧</button></td>
+            <td>%d</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%d</td>
+        </tr>";
+
+        if (isset($_POST['bt_search'])) {
+            $author    = e($_POST['author']);
+            $publisher = e($_POST['publisher']);
+
+            $where = "";
+            if (mb_strlen($author))
+                $where .= "author='".$author."'";
+            if (mb_strlen($publisher)) {
+                if (mb_strlen($where))
+                    $where .= " && ";
+                $where .= "publisher='".$publisher."'";
+            }
+
+            //DB問い合わせ (※検索条件がある場合のみ)
+            if (mb_strlen($where)) {
+                $ret = readTbl($tblName, $where, 'ORDER BY date');
+                if ($ret != FALSE) {
+                    $count = 1; //Index値
+                    foreach ($ret as $value) {
+                        //HTML作成
+                        $strSearchResult .= sprintf($format, (int)$value['idx'], $count,
+                                                    $value['date'], $value['title'], $value['author'], 
+                                                    $value['publisher'], $value['recommend']);
+                        $count += 1;
+                    }
+                }    
+            }
+        }       
+    }
+
 
     //年度毎の件数をTable表示
     {
@@ -48,6 +93,21 @@
         <input type="reset" value="取消">
         <input type="submit" name="bt_search" value="検索">
     </form>
+
+    <table>
+        <tr>
+            <th></th>
+            <th>No.</th>
+            <th>日付</th>
+            <th>タイトル</th>
+            <th>著者</th>
+            <th>出版社</th>
+            <th>評価</th>
+        </tr>
+        <?php echo $strSearchResult; ?>
+
+    </table>
+
 
     <p>年度毎の件数</p>
     <table>
